97b8fe63ebd6b6c17054919c23b4df86fbe5e630
dengyt <dengyt@chinawiserv.com>
2018-10-24 10:26:09 +0800

    添加路由中间介

--- myapp/routes/users.js
+++ myapp/routes/users.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
-
+//路由中间介
+router.use(function timeLog(req, res, next) {
+  console.log('Time: ', Date.now());
+  next();
+});
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
-
 module.exports = router;
