df8e84795102c76fc1c2688bd0edfd8d74fbaee3
dengyt <dengyt@chinawiserv.com>
2018-10-24 10:28:21 +0800

    对一个路径上的所有请求添加中间介

--- myapp/routes/users.js
+++ myapp/routes/users.js
@@ -5,6 +5,11 @@ router.use(function timeLog(req, res, next) {
   console.log('Time: ', Date.now());
   next();
 });
+//对一个路径上的所有请求添加中间介
+router.all('/',function(req,res,next){
+  console.log("触发请求");
+  next();
+});
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
